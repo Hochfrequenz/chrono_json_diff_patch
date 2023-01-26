1823acc52fe9b79b376beeaa1c41571fa0444e78
konstantin <konstantin.klein@hochfrequenz.de>
2023-01-26 11:48:00 +0100

    format

--- ChronoJsonDiffPatch/ChronoJsonDiffPatchTests/TimeRangePatchCollectionTests.cs
+++ ChronoJsonDiffPatch/ChronoJsonDiffPatchTests/TimeRangePatchCollectionTests.cs
@@ -9,7 +9,7 @@ public class TimeRangePatchCollectionTests
     [Fact]
     public void Test_Contains()
     {
-        var trpA = new TimeRangePatch(patch: null, from: new DateTimeOffset(2022, 1, 1, 0, 0, 0, TimeSpan.Zero), to:new DateTimeOffset(2023,1,1,0,0,0,TimeSpan.Zero));
+        var trpA = new TimeRangePatch(patch: null, from: new DateTimeOffset(2022, 1, 1, 0, 0, 0, TimeSpan.Zero), to: new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero));
         var trpB = new TimeRangePatch(patch: null, from: new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero));
         var trpCollection = new TimeRangePatchChain(new[] { trpA, trpB });
         trpCollection.Contains(new DateTimeOffset(2022, 1, 1, 0, 0, 0, TimeSpan.Zero)).Should().BeTrue();
@@ -20,7 +20,7 @@ public class TimeRangePatchCollectionTests
     [Fact]
     public void Test_Contains_With_Grace()
     {
-        var trpA = new TimeRangePatch(patch: null, from: new DateTimeOffset(2022, 1, 1, 0, 0, 0, TimeSpan.Zero), to:new DateTimeOffset(2023,1,1,0,0,0,TimeSpan.Zero));
+        var trpA = new TimeRangePatch(patch: null, from: new DateTimeOffset(2022, 1, 1, 0, 0, 0, TimeSpan.Zero), to: new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero));
         var trpB = new TimeRangePatch(patch: null, from: new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero));
         var trpCollection = new TimeRangePatchChain(new[] { trpA, trpB });
 
