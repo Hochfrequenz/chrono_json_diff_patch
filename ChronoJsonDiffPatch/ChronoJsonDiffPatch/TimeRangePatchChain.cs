3ccc79d803987bf950a67dbee23dcae4c34b0cbc
konstantin <konstantin.klein@hochfrequenz.de>
2023-01-27 11:27:07 +0100

    fix broken docstring

--- ChronoJsonDiffPatch/ChronoJsonDiffPatch/TimeRangePatchChain.cs
+++ ChronoJsonDiffPatch/ChronoJsonDiffPatch/TimeRangePatchChain.cs
@@ -12,7 +12,7 @@ namespace ChronoJsonDiffPatch;
 /// * There is a patchA at dtA that puts our entity in state A.
 /// * There is a patchB at dtB that puts our entity in state B. 
 /// If the patching order is now: (patchB, patchA), what is the desired state at dtB?
-/// This enums allows you to distinguish the behaviour of <see cref="TimeRangePatchChain{TEntity}.Add(TEntity,TEntity,System.DateTimeOffset)"/>
+/// This enums allows you to distinguish the behaviour of <see cref="TimeRangePatchChain{TEntity}.Add(TEntity,TEntity,System.DateTimeOffset,System.Nullable{ChronoJsonDiffPatch.FuturePatchBehaviour})"/>
 /// </summary>
 public enum FuturePatchBehaviour
 {
