27d4ebb5fe4f388f34ad310b1b91bd5ec948677c
konstantin <konstantin.klein@hochfrequenz.de>
2023-02-13 15:48:45 +0100

    Move classes into separate files (#29)

---
+++ ChronoJsonDiffPatch/ChronoJsonDiffPatch/FuturePatchBehaviour.cs
@@ -0,0 +1,23 @@
+﻿namespace ChronoJsonDiffPatch;
+
+/// <summary>
+/// Assume
+/// * There are two key dates: dtA and dtB
+/// * dtB &gt; dtA
+/// * There is a patchA at dtA that puts our entity in state A (assuming you use <see cref="PatchingDirection.ParallelWithTime"/>).
+/// * There is a patchB at dtB that puts our entity in state B (assuming you use <see cref="PatchingDirection.ParallelWithTime"/>).
+/// If the patching order is now: (patchB, patchA), what is the desired state at dtB?
+/// This enums allows you to distinguish the behaviour of <see cref="TimeRangePatchChain{TEntity}.Add(TEntity,TEntity,System.DateTimeOffset,System.Nullable{ChronoJsonDiffPatch.FuturePatchBehaviour})"/>
+/// </summary>
+public enum FuturePatchBehaviour
+{
+    /// <summary>
+    /// The future (&gt;= dtB) shall have state A: (the previous future state B is overwritten)
+    /// </summary>
+    OverwriteTheFuture,
+
+    /// <summary>
+    /// The future (&gt;= dtB) shall have state B: the second patchA only lasts in the interval between [A,B)
+    /// </summary>
+    KeepTheFuture,
+}
--- ChronoJsonDiffPatch/ChronoJsonDiffPatch/TimeRangePatchChain.cs
+++ ChronoJsonDiffPatch/ChronoJsonDiffPatch/TimeRangePatchChain.cs
@@ -7,54 +7,6 @@ using Newtonsoft.Json.Linq;
 namespace ChronoJsonDiffPatch;
 
 /// <summary>
-/// Assume
-/// * There are two key dates: dtA and dtB
-/// * dtB &gt; dtA
-/// * There is a patchA at dtA that puts our entity in state A (assuming you use <see cref="PatchingDirection.ParallelWithTime"/>).
-/// * There is a patchB at dtB that puts our entity in state B (assuming you use <see cref="PatchingDirection.ParallelWithTime"/>).
-/// If the patching order is now: (patchB, patchA), what is the desired state at dtB?
-/// This enums allows you to distinguish the behaviour of <see cref="TimeRangePatchChain{TEntity}.Add(TEntity,TEntity,System.DateTimeOffset,System.Nullable{ChronoJsonDiffPatch.FuturePatchBehaviour})"/>
-/// </summary>
-public enum FuturePatchBehaviour
-{
-    /// <summary>
-    /// The future (&gt;= dtB) shall have state A: (the previous future state B is overwritten)
-    /// </summary>
-    OverwriteTheFuture,
-
-    /// <summary>
-    /// The future (&gt;= dtB) shall have state B: the second patchA only lasts in the interval between [A,B)
-    /// </summary>
-    KeepTheFuture,
-}
-
-/// <summary>
-/// Assume
-/// * There are two key dates: dtA and dtB
-/// * dtB &gt; dtA
-/// This enum describes if we model the <see cref="TimeRangePatchChain{TEntity}"/> as patches that are applied from earlier to later times or the other way around.
-/// </summary>
-public enum PatchingDirection
-{
-    /// <summary>
-    /// <see cref="TimeRangePatch.Patch"/>es are modelled as transitions from dtA to dtB.
-    /// You could also refer to this as "left to right" on a time axis.
-    /// </summary>
-    /// <remarks>This is the patching direction that feels "natural".</remarks>
-    ParallelWithTime,
-
-    /// <summary>
-    /// <see cref="TimeRangePatch.Patch"/>es are modelled as transitions from dtB to dtA.
-    /// You could also refer to this as "right to left" on a time axis.
-    /// </summary>
-    /// <remarks>
-    /// Although this doesn't feel as natural as <see cref="ParallelWithTime"/>, it might useful if you store changes to an entity in a database and you always want to have the most recent/youngest state as "full" entity and persist the past as patches.
-    /// Then you always start with the youngest entity and patch "backwards" until you reach the point in time/in the past in which you're interested. 
-    /// </remarks>
-    AntiParallelWithTime,
-}
-
-/// <summary>
 /// A collection of <see cref="TimeRangePatch"/>es that implements <see cref="ITimePeriodChain"/>
 /// </summary>
 /// <typeparam name="TEntity">a serializable entity whose changes shall be tracked as JsonDiffPatches</typeparam>
