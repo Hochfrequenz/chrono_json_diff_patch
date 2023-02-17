891b8d166f15ea23b14d5d652c1adf4c7e3b2419
konstantin <konstantin.klein@hochfrequenz.de>
2023-02-17 13:19:54 +0100

    ✨Introduce Chain Products that track the joined state of two entites

---
+++ ChronoJsonDiffPatch/ChronoJsonDiffPatch/ProductEntity.cs
@@ -0,0 +1,36 @@
+﻿namespace ChronoJsonDiffPatch;
+
+/// <summary>
+/// A ProductEntity is a composite of both an <typeparamref name="TEntityA"/> and a <typeparamref name="TEntityB"/>. 
+/// </summary>
+/// <remarks>This is a shortcut for a <see cref="Tuple{TEntityA, TEntityB, DateTimeOffset}"/></remarks>
+/// <typeparam name="TEntityA"></typeparam>
+/// <typeparam name="TEntityB"></typeparam>
+public class ProductEntity<TEntityA, TEntityB>
+{
+    /// <summary>
+    /// describes the date at which <typeparamref name="TEntityA"/> has state <see cref="EntityA"/> and <typeparamref name="TEntityB"/> has state <see cref="EntityB"/>
+    /// </summary>
+    public DateTimeOffset KeyDate { get; }
+    /// <summary>
+    /// an instance of <typeparamref name="TEntityA"/> @ <see cref="KeyDate"/>
+    /// </summary>
+    public TEntityA EntityA { get; }
+    /// <summary>
+    /// an instance of <typeparamref name="TEntityB"/> @ <see cref="KeyDate"/>
+    /// </summary>
+    public TEntityB EntityB { get; }
+
+    /// <summary>
+    /// initialize the product by providing the three relevant values
+    /// </summary>
+    /// <param name="entityA"><typeparamref name="TEntityA"/> at <paramref name="keyDate"/></param>
+    /// <param name="entityB"><typeparamref name="TEntityB"/> at <paramref name="keyDate"/></param>
+    /// <param name="keyDate">date which is relevant</param>
+    public ProductEntity(TEntityA entityA, TEntityB entityB, DateTimeOffset keyDate)
+    {
+        EntityA = entityA;
+        EntityB = entityB;
+        KeyDate = keyDate;
+    }
+}
---
+++ ChronoJsonDiffPatch/ChronoJsonDiffPatch/TimeRangePatchChainProduct.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace ChronoJsonDiffPatch;
+
+/// <summary>
+/// Two <see cref="TimeRangePatchChain{TEntity}"/>s can be combined to form "TimeRangePatchChainProduct".
+/// The product can be used to create a <see cref="ProductEntity{TEntityA,TEntityB}"/> which tracks changes of both <typeparamref name="TEntityA"/> and  <typeparamref name="TEntityB"/>.
+/// </summary>
+public class TimeRangePatchChainProduct<TEntityA, TEntityB>
+{
+    private readonly TimeRangePatchChain<TEntityA> _chainA;
+    private readonly TimeRangePatchChain<TEntityB> _chainB;
+    private readonly TEntityA _initialStateOfA;
+    private readonly TEntityB _initialStateOfB;
+
+    /// <summary>
+    /// initialize the product by providing both chains and the respective initial entity states
+    /// </summary>
+    /// <param name="chainA"></param>
+    /// <param name="initialStateOfA"></param>
+    /// <param name="chainB"></param>
+    /// <param name="initialStateOfB"></param>
+    public TimeRangePatchChainProduct(TimeRangePatchChain<TEntityA> chainA, TEntityA initialStateOfA, TimeRangePatchChain<TEntityB> chainB, TEntityB initialStateOfB)
+    {
+        _chainA = chainA;
+        _chainB = chainB;
+        _initialStateOfA = initialStateOfA;
+        _initialStateOfB = initialStateOfB;
+    }
+
+    [Pure]
+    private ProductEntity<TEntityA, TEntityB> PatchToDate(DateTimeOffset keyDate)
+    {
+        var stateOfEntityA = _chainA.PatchToDate(_initialStateOfA, keyDate);
+        var stateOfEntityB = _chainB.PatchToDate(_initialStateOfB, keyDate);
+        return new ProductEntity<TEntityA, TEntityB>(stateOfEntityA, stateOfEntityB, keyDate);
+    }
+
+    /// <summary>
+    /// returns the joint history of both chains.
+    /// </summary>
+    /// <returns>an enumerable that contains entries for every relevant date which there is in either or both of the chains</returns>
+    [Pure]
+    public IEnumerable<ProductEntity<TEntityA, TEntityB>> GetAll()
+    {
+        var result = _chainA.GetAll().Select(trp => trp.Start).Union(_chainB.GetAll().Select(trp => trp.Start))
+            .Where(dt => dt != DateTime.MinValue)
+            .Where(dt => dt != DateTime.MaxValue)
+            .Distinct()
+            .Order()
+            .Select(keyDate => PatchToDate(keyDate));
+        return result;
+    }
+}
