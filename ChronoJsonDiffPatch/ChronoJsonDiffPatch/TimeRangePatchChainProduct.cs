ef8453266c118b8b906efc3536f902e56f1794bc
konstantin <konstantin.klein@hochfrequenz.de>
2023-02-19 10:11:37 +0100

    ✨Introduce Chain Products that track the joined state of two entites (#30)

---
+++ ChronoJsonDiffPatch/ChronoJsonDiffPatch/TimeRangePatchChainProduct.cs
@@ -0,0 +1,20 @@
+﻿namespace ChronoJsonDiffPatch;
+
+/// <summary>
+/// Two <see cref="TimeRangePatchChain{TEntity}"/>s can be combined to form "TimeRangePatchChainProduct".
+/// The product can be used to create a <see cref="ProductEntity{TEntityA,TEntityB}"/> which tracks changes of both <typeparamref name="TEntityA"/> and <typeparamref name="TEntityB"/>.
+/// </summary>
+public class TimeRangePatchChainProduct<TEntityA, TEntityB> : GenericTimeRangePatchChainProduct<TwoTypeChainAndEntities<TEntityA, TEntityB>, ProductEntity<TEntityA, TEntityB>>
+{
+    /// <summary>
+    /// initialize by providing the raw chains and entities involved
+    /// </summary>
+    /// <param name="entityA"></param>
+    /// <param name="chainA"></param>
+    /// <param name="entityB"></param>
+    /// <param name="chainB"></param>
+    public TimeRangePatchChainProduct(TimeRangePatchChain<TEntityA> chainA, TEntityA entityA, TimeRangePatchChain<TEntityB> chainB, TEntityB entityB) : base(
+        new TwoTypeChainAndEntities<TEntityA, TEntityB>(chainA, entityA, chainB, entityB))
+    {
+    }
+}
---
+++ ChronoJsonDiffPatch/ChronoJsonDiffPatch/TwoTypeChainAndEntities.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace ChronoJsonDiffPatch;
+
+/// <summary>
+/// type that encapsulates state and chain of two entities <typeparamref name="TEntityA"/> and <typeparamref name="TEntityB"/>.
+/// </summary>
+/// <typeparam name="TEntityA"></typeparam>
+/// <typeparam name="TEntityB"></typeparam>
+public class TwoTypeChainAndEntities<TEntityA, TEntityB> : IChainsAndEntities<ProductEntity<TEntityA, TEntityB>>
+{
+    private readonly TimeRangePatchChain<TEntityA> _chainA;
+    private readonly TimeRangePatchChain<TEntityB> _chainB;
+    private readonly TEntityA _initialStateOfA;
+    private readonly TEntityB _initialStateOfB;
+
+    /// <summary>
+    /// instantiate by providing both initial entities <paramref name="initialStateOfA"/>, <paramref name="initialStateOfB"/> as well as the respective chains <paramref name="chainA"/>, <paramref name="chainB"/>
+    /// </summary>
+    /// <param name="chainA"></param>
+    /// <param name="initialStateOfA"></param>
+    /// <param name="chainB"></param>
+    /// <param name="initialStateOfB"></param>
+    public TwoTypeChainAndEntities(TimeRangePatchChain<TEntityA> chainA, TEntityA initialStateOfA, TimeRangePatchChain<TEntityB> chainB, TEntityB initialStateOfB)
+    {
+        _chainA = chainA;
+        _chainB = chainB;
+        _initialStateOfA = initialStateOfA;
+        _initialStateOfB = initialStateOfB;
+    }
+
+    /// <inheritdoc />
+    [Pure]
+    public ProductEntity<TEntityA, TEntityB> ToProductEntity(DateTimeOffset keyDate)
+    {
+        var stateOfEntityA = _chainA.PatchToDate(_initialStateOfA, keyDate);
+        var stateOfEntityB = _chainB.PatchToDate(_initialStateOfB, keyDate);
+        return new ProductEntity<TEntityA, TEntityB>(stateOfEntityA, stateOfEntityB, keyDate);
+    }
+
+    /// <inheritdoc />
+    [Pure]
+    public IEnumerable<DateTime> GetPatchingDates()
+    {
+        return _chainA.GetAll().Select(trp => trp.Start)
+            .Union(_chainB.GetAll().Select(trp => trp.Start))
+            .Distinct();
+    }
+}
